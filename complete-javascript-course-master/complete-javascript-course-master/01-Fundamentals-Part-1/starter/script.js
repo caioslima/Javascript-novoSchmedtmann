945b353016b5c5244a339f6ef5c77882e7db2579
caioslima <caiosl2020@outlook.com>
2023-06-18 02:03:58 -0300

    Update script.js

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -77,4 +77,47 @@ if (birthYear <= 2000) {
 }
 console.log(century);
 */
+/*
+const favorite = Number(prompt("What's your age?"));
+console.log(favorite, typeof favorite);
 
+if (favorite > 60) {
+    console.log("Meus parabens!!! O(A) senhor(a) ja viveu quase toda sua vida, mas ainda tem muita coisa pra aprender");
+} else if (favorite > 29) {
+    console.log("Meus parabens!!! O(a) senhor(a) ja viveu metade da sua vida");
+} else if (favorite >= 18) {
+    console.log("Meus parabens!!! O(a) senhor(a) ja virou um adulto e pode sofrer coisas de adulto");
+} else if (favorite < 18) {
+    console.log("Meus parabens!!! O(a) senhor(a) acabou de começar a sua vida");
+}
+*/
+/*
+const day = 'Saturday';
+
+if (day === 'Monday') {
+    console.log(`Plan course structure`);
+    console.log(`Go to coding meetup`);
+} else if (day === 'Tuesday') {
+    console.log(`Prepare theory videos`);
+} else if (day === 'Wednesday' || day === 'Thursday') {
+    console.log(`Write code examples`);
+} else if (day === 'Friday') {
+    console.log(`Record videos`);
+} else if (day === 'Saturday' || day === 'Sunday') {
+    console.log(`Enjoy the weekend 😎`);
+} else {
+    console.log(`Not a valid day!`);
+}
+*/
+
+//Challenge#4
+const bill = 275;
+let tip;
+
+bill >= 50 && bill <= 300 ? tip = bill * 0.15 : tip = bill * .20;
+
+console.log(tip);
+
+const msgTip = `The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}.`
+
+console.log(msgTip);
