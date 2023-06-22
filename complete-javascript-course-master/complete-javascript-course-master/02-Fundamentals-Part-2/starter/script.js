8d4c0b849cc622c58fb73524ba7385d32f421351
caioslima <caiosl2020@outlook.com>
2023-06-22 02:31:12 -0300

    Update script.js

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -418,6 +418,7 @@ while (rep <= 10) {
     rep++;
 }
 */
+/*
 let dice = Math.trunc(Math.random() * 6) + 1;
 console.log(dice);
 
@@ -426,3 +427,33 @@ while (dice !== 6) {
     dice = Math.trunc(Math.random() * 6) + 1;
     if (dice === 6) console.log('Loop is about to end...')
 }
+*/
+
+//Challenge#4
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tips[i]);
+    console.log(bills[i], '+', tips[i], '=', totals[i]);
+}
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
