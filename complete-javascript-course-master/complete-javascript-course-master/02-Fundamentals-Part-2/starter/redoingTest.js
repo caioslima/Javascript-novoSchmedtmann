7252ca5538aa9d8a23a203df04dca08e30ba796e
caioslima <caiosl2020@outlook.com>
2023-06-23 18:21:44 -0300

    Update redoingTest.js

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/redoingTest.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/redoingTest.js
@@ -3,12 +3,19 @@ const chinese = 1200000000;
 const spanish = 700000000;
 const ensglish = 600000000;
 //population of the world
-const china = 1250000000;
-const portugal = 11500000;
-const brazil = 212000000;
+const countrys = {
+  china: 1250000000,
+  portugal: 11500000,
+  brazil: 212000000,
+};
 
-if (country > 33000000) {
-  console.log(`${country}'s population is above average`);
-} else {
-  console.log(`${country}'s population is below average`);
+const country = [20000000, 35000000, 541000000, 24000000];
+
+for (let i = 0; i < country.length; i++) {
+  if (country[i] > 33000000) {
+    console.log(`${country[i]}'s population is above average`);
+  } else {
+    console.log(`${country[i]}'s population is below average`);
+  }
 }
+console.log(countrys);
