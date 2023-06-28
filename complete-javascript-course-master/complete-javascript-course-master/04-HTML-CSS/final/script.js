1d235009a8d90c288ad0d9935718226e72a36fdd
caioslima <caiosl2020@outlook.com>
2023-06-28 01:07:09 -0300

    modal-open-close

---
+++ complete-javascript-course-master/complete-javascript-course-master/04-HTML-CSS/final/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+//Escreva um programa que solicite ao usuário um número inteiro positivo e, em seguida, exiba a sequência de Fibonacci correspondente até o número fornecido.
+
+//A sequência de Fibonacci é uma série de números em que cada número é a soma dos dois números anteriores. A sequência começa com 0 e 1.
