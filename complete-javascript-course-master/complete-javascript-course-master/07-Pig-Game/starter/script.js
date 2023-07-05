34f397230049cdbc3f350678741ddbe7ca8b77d3
caioslima <caiosl2020@outlook.com>
2023-07-05 01:02:54 -0300

    Update script.js

--- complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -6,6 +6,7 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -15,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const finalScores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   // 1. generating a random dice roll
@@ -26,25 +29,35 @@ btnRoll.addEventListener('click', function () {
   // 3. Check for rolled 1: if true
   if (dice !== 1) {
     //Add to current score
-    if (player0El.classList.contains('player--active')) {
-      currentScore += dice;
-      current0El.textContent = currentScore;
-    } else if (player1El.classList.contains('player--active')) {
-      currentScore += dice;
-      current1El.textContent = currentScore;
-    }
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // if (player0El.classList.contains('player--active')) {
+    //   currentScore += dice;
+    //   current0El.textContent = currentScore;
+    // } else if (player1El.classList.contains('player--active')) {
+    //   currentScore += dice;
+    //   current1El.textContent = currentScore;
+    // }
   } else {
     // Switch to next player
-    if (player0El.classList.contains('player--active')) {
-      player0El.classList.remove('player--active');
-      player1El.classList.add('player--active');
-      current0El.textContent = 0;
-      currentScore = 0;
-    } else if (player1El.classList.contains('player--active')) {
-      player1El.classList.remove('player--active');
-      player0El.classList.add('player--active');
-      current1El.textContent = 0;
-      currentScore = 0;
-    }
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+
+    // if (player0El.classList.contains('player--active')) {
+    //   player0El.classList.remove('player--active');
+    //   player1El.classList.add('player--active');
+    //   current0El.textContent = 0;
+    //   currentScore = 0;
+    // } else if (player1El.classList.contains('player--active')) {
+    //   player1El.classList.remove('player--active');
+    //   player0El.classList.add('player--active');
+    //   current1El.textContent = 0;
+    //   currentScore = 0;
+    // }
   }
 });
