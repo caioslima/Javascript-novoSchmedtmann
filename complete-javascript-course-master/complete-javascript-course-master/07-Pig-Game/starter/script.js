04be07a5988bda49c1329261cf4eb5125270ae51
caioslima <caiosl2020@outlook.com>
2023-08-01 23:42:59 -0300

    Update script.js

--- complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -14,14 +14,24 @@ const btnHold = document.querySelector('.btn--hold');
 
 const currentScoreEl = document.querySelector('.current-score');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+//Starting conditions
+const init = function () {
+  const scores = [0, 0];
+  let currentScore = 0;
+  let activePlayer = 0;
+  let playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,6 +41,7 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
+//Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. generating a random dice roll
@@ -78,16 +89,7 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.remove('player--active');
-  player1El.classList.remove('player--active');
-});
+btnNew.addEventListener('click', function () {});
 
 // btnNew.addEventListener('click', function () {
 //   // scores[0] = scores[0] - scores[0];
