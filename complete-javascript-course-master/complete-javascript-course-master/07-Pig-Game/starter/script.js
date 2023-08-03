723971dbeba5e1ee23a77fd0d0cb954beef61157
caioslima <caiosl2020@outlook.com>
2023-08-02 22:18:19 -0300

    Update script.js

--- complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -15,11 +15,13 @@ const btnHold = document.querySelector('.btn--hold');
 const currentScoreEl = document.querySelector('.current-score');
 
 //Starting conditions
+let scores, currentScore, activePlayer, playing;
+
 const init = function () {
-  const scores = [0, 0];
-  let currentScore = 0;
-  let activePlayer = 0;
-  let playing = true;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
   score0El.textContent = 0;
   score1El.textContent = 0;
@@ -32,6 +34,7 @@ const init = function () {
   player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
 };
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
